b69449b481fe616c6f2cc71716a871b9cc994046
viviieverse <viviie.tangwei@gmail.com>
2026-05-11 12:53:09 +0800

    Add files via upload

--- examples/ai-test/main.cpp
+++ examples/ai-test/main.cpp
@@ -1,7 +1,8 @@
+
 #include <QCoreApplication>
 #include <QDebug>
 
-#include <PhantomDrive/ai/AIOpponentFSM.h>
+#include <PhantomDrive/ai/StrategicAI.h>
 
 using namespace PhantomDrive;
 
@@ -9,9 +10,33 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    AIOpponentFSM ai;
+    StrategicAI ai;
+    QList<Waypoint> points;
 
-    ai.update(0.016f);
+    Waypoint wp1;
+    wp1.x = 100;
+    wp1.y = 50;
+
+    Waypoint wp2;
+    wp2.x = -100;
+    wp2.y = 20;
+
+    points.append(wp1);
+    points.append(wp2);
+
+    ai.setWaypoints(points);
+
+    for (int i = 0; i < 30; i++)
+    {
+        ai.update(0.016f);
+
+        AIDecision decision = ai.makeDecision();
+
+        qDebug() << "Step:" << i;
+        qDebug() << "Throttle:" << decision.throttle;
+        qDebug() << "Brake:" << decision.brake;
+        qDebug() << "Steering:" << decision.steering;
+    }
 
     AIDecision decision = ai.makeDecision();
 
